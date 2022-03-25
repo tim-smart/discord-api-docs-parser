74cee72b44b1174374d88d0dc598c82fafac5fbc
Tim Smart <tim.smart@arisechurch.com>
2022-03-25 13:15:04 +1300

    add Locale support

--- src/additional.ts
+++ src/additional.ts
@@ -1,6 +1,53 @@
 import { Structure } from "./structures";
 
+const locales = [
+  { code: "da", name: "Danish" },
+  { code: "de", name: "German" },
+  { code: "en-GB", name: "English," },
+  { code: "en-US", name: "English," },
+  { code: "es-ES", name: "Spanish" },
+  { code: "fr", name: "French" },
+  { code: "hr", name: "Croatian" },
+  { code: "it", name: "Italian" },
+  { code: "lt", name: "Lithuanian" },
+  { code: "hu", name: "Hungarian" },
+  { code: "nl", name: "Dutch" },
+  { code: "no", name: "Norwegian" },
+  { code: "pl", name: "Polish" },
+  { code: "pt-BR", name: "Portuguese, Brazilian" },
+  { code: "ro", name: "Romanian, Romania" },
+  { code: "fi", name: "Finnish" },
+  { code: "sv-SE", name: "Swedish" },
+  { code: "vi", name: "Vietnamese" },
+  { code: "tr", name: "Turkish" },
+  { code: "cs", name: "Czech" },
+  { code: "el", name: "Greek" },
+  { code: "bg", name: "Bulgarian" },
+  { code: "ru", name: "Russian" },
+  { code: "uk", name: "Ukrainian" },
+  { code: "hi", name: "Hindi" },
+  { code: "th", name: "Thai" },
+  { code: "zh-CN", name: "Chinese, China" },
+  { code: "ja", name: "Japanese" },
+  { code: "zh-TW", name: "Chinese, Taiwan" },
+  { code: "ko", name: "Korean" },
+];
+
 export const structures = (): Structure[] => [
+  {
+    identifier: "Locale",
+    fields: locales.map(({ name, code }) => ({
+      name: code,
+      optional: true,
+      type: {
+        identifier: "string",
+        nullable: false,
+        array: false,
+        snowflakeMap: false,
+      },
+      description: name,
+    })),
+  },
   {
     identifier: "UnavailableGuild",
     fields: [
